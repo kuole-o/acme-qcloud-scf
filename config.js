a5bfd3184a42c34ffcb733c4d65c647ddf30b739
GuoLe <guole.fun@qq.com>
2023-10-27 20:56:49 +0800

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    isDebug: true,
+    isDebug: true, // 验证完成后，此项必须配置为 false，否则申请的是测试证书，浏览器会报错“无效证书”
     email: process.env.Email,  // 你的邮箱
     domain: process.env.Domain, // 需要生成证书的根域名，最终生成通配符证书
     qcloudSecretId: process.env.Tcb_SecretId, // 腾讯云 SecretId, https://console.cloud.tencent.com/cam/capi
